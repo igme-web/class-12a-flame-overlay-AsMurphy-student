73d1e597a6b295a8ba2f807c2b13e21c1be56245
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-11-13 11:18:33 -0500

    Completed step 3 of part 1

--- week12a_flame_overlay_starter/lib/main.dart
+++ week12a_flame_overlay_starter/lib/main.dart
@@ -9,11 +9,20 @@ import 'overlay_pause.dart';
 import 'overlay_info.dart';
 import 'overlay_settings.dart';
 
+import 'package:provider/provider.dart';
+import 'game_provider.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Flame.device.fullScreen();
 
-  runApp(const MainApp());
+  // runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => GameProvider(),
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
